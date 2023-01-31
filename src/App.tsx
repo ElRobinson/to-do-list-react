2a732ca3bbf20710b6f5eb704406acb85718c7a9
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2023-01-30 21:28:37 -0300

    feat: added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { Header } from './components/Header';
 import './styles/global.css';
 
 function App() {
 
 
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    <Header />
+    </>
   )
 }
 
